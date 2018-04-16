6bf78b9986bc85abab6bec620933c1671e6a57c9
Matthew Root <root.matthew@gmail.com>
2018-04-16 09:48:50 -0700

    Add minimal webpack config

---
+++ src/index.js
@@ -0,0 +1,1 @@
+console.log('Successful bundle reference');
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['build'])
+  ]
+};
