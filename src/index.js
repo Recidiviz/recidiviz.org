35185cb3c13fd5043ef6fc4023845b98252c72f3
Matthew Root <root.matthew@gmail.com>
2018-04-17 18:23:51 -0700

    Add eslint for linting JS, use Airbnb styleguide plugins

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let document;
+
 ReactDOM.render(
   <h1>Hello, React!</h1>,
-  document.getElementById('app')
+  document.getElementById('app'),
 );
