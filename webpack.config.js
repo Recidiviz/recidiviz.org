7f7064851c6c7c84ffab5da80f81646c19840eb3
Matthew Root <root.matthew@gmail.com>
2018-04-22 08:42:07 -0700

    Update webpack build config for fonts and images

--- webpack.config.js
+++ webpack.config.js
@@ -29,13 +29,23 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]-[hash].[ext]',
+            },
+          },
         ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          'file-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'fonts/[name]-[hash].[ext]',
+            },
+          },
         ],
       },
     ],
